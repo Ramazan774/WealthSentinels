288f900a0f92f14b8bccd1eca7afd5b39118bea7
Ramazan774 <ramazan.ramazanov@glion.ch>
2025-12-09 15:42:20 -0600

    Fix DATABASE_URL parsing - default to port 5432

--- WealthSentinels.api/Program.cs
+++ WealthSentinels.api/Program.cs
@@ -67,7 +67,8 @@ string ConvertDatabaseUrlToConnectionString(string rawUrl)
 {
     var uri = new Uri(rawUrl);
     var userInfo = uri.UserInfo.Split(':');
-    return $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
+    var port = uri.Port > 0 ? uri.Port : 5432; // Default to 5432 if port not specified
+    return $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};Username={userInfo[0]};Password={userInfo[1]};SSL Mode=Require;Trust Server Certificate=true";
 }
 
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
